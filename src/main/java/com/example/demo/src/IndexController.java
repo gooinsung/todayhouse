19e8e75e272ee85c54c952e32f1b49b26359d910
gooinsung <gooinsung96@gmail.com>
2023-01-10 20:22:40 +0900

    유저 프로필 수정시, 닉네임만, 사진만, 닉네임과사진 다 수정할수 있도록 수정

--- src/main/java/com/example/demo/src/IndexController.java
+++ src/main/java/com/example/demo/src/IndexController.java
@@ -57,7 +57,7 @@ public class IndexController {
     }
 
     // 회원 가입 API(17)
-    @PostMapping("/join")
+    @PostMapping("/sign-up")
     public BaseResponse<String> join(@Validated @RequestBody PostJoinRequest postJoinRequest) throws BaseException {
         logger.info("IndexController 내 17번 API 실행");
         String result="회원 가입 실패";
@@ -68,7 +68,7 @@ public class IndexController {
     }
 
     // 로그인 API(18)
-    @PostMapping("/login")
+    @PostMapping("/sign-in")
     public BaseResponse<String> login(@Validated @RequestBody PostLoginRequest postLoginRequest, HttpServletResponse response) throws BaseException{
         logger.info("IndexController 내 18번 API 실행");
         String result="로그인 실패";
